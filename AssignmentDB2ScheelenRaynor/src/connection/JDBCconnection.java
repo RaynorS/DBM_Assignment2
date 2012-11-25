b24e3691c86bf11d07cd4c63d5895126b3d81e58
RaynorS <rsregsonline@gmail.com>
2012-11-25 07:59:45 +0100

    Completed exercise Maybe extra debugging necessary

--- AssignmentDB2ScheelenRaynor/src/connection/JDBCconnection.java
+++ AssignmentDB2ScheelenRaynor/src/connection/JDBCconnection.java
@@ -21,59 +21,111 @@ public class JDBCconnection {
 		}
 	}
 	public static void main(String[] args) {
-		String uname = null;
-		String psswrd = null;
+		String uname = "student";
+		String psswrd = "student";
+		Integer courseNumber = 1;
+		String query = null;
+		String courseName = "";
+		Boolean courseExists = false;
 		/* Location of the database */
 		String url = "jdbc:oracle:thin:@134.58.95.246:1521:XE";
-		/* Sample query */
-		String query = "SELECT FName, LName FROM Assignment_DB.Student";
-		/* Reading log-in data (username and password) */
-		try
-		{
-			BufferedReader br1 = new BufferedReader (new InputStreamReader(System.in));
-			System.out.print("Enter your username on OracleXE: ");
-			uname = br1.readLine();
-			BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Enter your password on OracleXE: ");
-			psswrd = br2.readLine();
-		}
-		catch (IOException e)
-		{
-			System.out.print("Failed to get uname/passwd");
-			System.out.println(":" + e.getMessage());
-			System.exit(1);
-		}
-		try
-		{
-			/* Connect to Oracle database */
-			Connection conn = DriverManager.getConnection(url, uname, psswrd);
-			System.out.println("Connection established...");
-			System.out.println();
-			/* Create statement */
-			Statement stmt = conn.createStatement();
-			/* Execute the query */
-			ResultSet rs = stmt.executeQuery(query);
-			/* Output */
-			System.out.println(query);
-			System.out.println("First name // Last name");
-			System.out.println("------------------------------");
-			while (rs.next())
+		
+		
+		
+		while (courseNumber != 0){
+			try
 			{
-				System.out.print(rs.getString(1));
-				System.out.print((" // "));
-				System.out.println(rs.getString(2));
+				/* Connect to Oracle database */
+				Connection conn = DriverManager.getConnection(url, uname, psswrd);
+				/* Create statement */
+				Statement stmt = conn.createStatement(
+					ResultSet.TYPE_SCROLL_SENSITIVE,
+					ResultSet.CONCUR_UPDATABLE
+				);
+				ResultSet rs = null;
+				/* Reading course number */
+				while (courseExists == false)
+				try
+				{
+					BufferedReader br1 = new BufferedReader (new InputStreamReader(System.in));
+					System.out.print("Please enter the course number: ");
+					courseNumber = Integer.parseInt(br1.readLine());
+					/* Execute the query */
+					if (courseNumber == 0) {
+						
+						break;
+					}
+					query = "SELECT CourseName, Instructor FROM Assignment_DB.Course WHERE CourseID = " + courseNumber;
+					rs = stmt.executeQuery(query);
+					/* Output */
+					System.out.println();
+					if (rs.next())
+					{
+						rs.first();
+						courseName = rs.getString(1);
+						String title = rs.getString(1) + " taught by " + rs.getString(2);
+						System.out.println(title);
+						String repeated = new String(new char[title.length()]).replace("\0", "*");
+						System.out.println(repeated);
+						System.out.println();
+						courseExists = true;
+					} else {
+						System.out.println("Course number does not exist. Please try again.");
+						courseExists = false;						
+					}
+				}
+				
+				catch (IOException e)
+				{
+					System.out.print("Failed to get course number!");
+					System.out.println(":" + e.getMessage());
+					System.exit(1);
+				}
+				if (courseNumber == 0) {
+					break;
+				}
+				courseExists = false;
+				
+				query = "SELECT S.FName, S.Lname, nvl(to_char(E.Grade),'***') FROM Assignment_DB.Enrolled E LEFT JOIN Assignment_DB.Student S ON S.StudentID = E.StudentID WHERE CourseID = " + courseNumber;
+				rs.close();
+				rs = stmt.executeQuery(query);
+				System.out.println("First name // Last name // Grade");
+				System.out.println("--------------------------------");
+				while (rs.next())
+				{
+					System.out.println(rs.getString(1) + " // " + rs.getString(2) + " // " + rs.getString(3));
+				}
+				query = "SELECT avg(E.Grade), E.CourseID FROM Assignment_DB.Enrolled E WHERE E.CourseID = " + courseNumber  + " GROUP BY E.CourseID" ;
+				rs.close();
+				rs = stmt.executeQuery(query);
+				rs.first();
+				System.out.println();
+				System.out.println("The average grade of " + courseName + " is " + rs.getString(1) +".");
+				query = "SELECT count(AssignmentID), CourseID FROM Assignment_DB.Assignment WHERE CourseID = " + courseNumber  + " GROUP BY CourseID" ;
+				rs.close();
+				rs = stmt.executeQuery(query);
+				if (rs.next())
+				{
+					System.out.println(courseName + " has " + rs.getString(1) + " assignment(s).");
+					
+				}
+				else {
+					System.out.println(courseName + " has no assignment(s).");
+				}
+				System.out.println();
+				rs.close();
+				stmt.close();
+				conn.close();
 			}
-			rs.close();
-			stmt.close();
-			conn.close();
+			catch (SQLException e)
+			{
+				System.out.println("SQL Exception: ");
+				System.err.println(e.getMessage());
 			}
-		catch (SQLException e)
-		{
-			System.out.println("SQL Exception: ");
-			System.err.println(e.getMessage());
 		}
+		
+		System.out.println("End of Session");
 	}
-	
 	
 
 }
